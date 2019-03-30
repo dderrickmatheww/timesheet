2f6e1c1be1e8d17888458e0e5469f50c40e7b1de
Robert Butler <robwbutler@gmail.com>
2019-03-30 11:49:32 -0400

    all the stuff fixed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,30 @@
 // Initialize Firebase
+
+var name = "";
+var role = "";
+var date = "";
+var rate = "";
+
 var config = {
-    apiKey: "AIzaSyArsOzYgCmZm6VGmQPfRnKJSZgciww10C0",
-    authDomain: "titlesheet-27591.firebaseapp.com",
-    databaseURL: "https://titlesheet-27591.firebaseio.com",
-    projectId: "titlesheet-27591",
-    storageBucket: "titlesheet-27591.appspot.com",
-    messagingSenderId: "609136145728"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyArsOzYgCmZm6VGmQPfRnKJSZgciww10C0",
+  authDomain: "titlesheet-27591.firebaseapp.com",
+  databaseURL: "https://titlesheet-27591.firebaseio.com",
+  projectId: "titlesheet-27591",
+  storageBucket: "titlesheet-27591.appspot.com",
+  messagingSenderId: "609136145728"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database()
 
 $("#addemployee").on("click", function(){
-    var name = $("#name-input").val().trim()
-    var role = $("#role-input").val().trim()
-    var date = $("#date-input").val().trim()
-    var rate = $("#rate-input").val().trim()
+    name = $("#name-input").val().trim()
+    role = $("#role-input").val().trim()
+    date = $("#date-input").val().trim()
+    rate = $("#rate-input").val().trim()
+
+    console.log(name, role, date, rate)
 
     var t = $("<tr>")
     var nt = $("<td>")
@@ -39,8 +50,8 @@ $("#addemployee").on("click", function(){
     t.append(nt)
     t.append(rt)
     t.append(dt)
-    t.append(rat)
     t.append(mont)
+    t.append(rat)
     t.append(tott)
 
     $("#employees").append(t)
@@ -50,8 +61,8 @@ $("#addemployee").on("click", function(){
 })
 
 database.ref().push({
-    name: name,
-    role: role,
-    date: firebase.database.ServerValue.TIMESTAMP,
-    rate: rate
-  });
+  name: name,
+  role: role,
+  date: date,
+  rate: rate,
+});
